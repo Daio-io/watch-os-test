3f65f71ee2d6c3b06af6f9fbb0fce466bbf76bce
Daio-io <dai.williams@bbc.co.uk>
2015-12-21 23:44:40 +0000

    Initial commit

---
+++ watchtest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  watchtest
+//
+//  Created by Dai Williams on 21/12/2015.
+//  Copyright © 2015 daio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
